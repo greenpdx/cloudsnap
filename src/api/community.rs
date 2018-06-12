009fce5d3b9950532e29f3fc6e98929dfefb0378
David Darrell <david.darrell@gmail.com>
2018-06-12 22:14:42 +0800

    Update actix-web to 0.6
    with2 has been removed from actix-web 0.7

--- src/api/community.rs
+++ src/api/community.rs
@@ -4,12 +4,14 @@ use futures::future::Future;
 use api::index::AppState;
 use model::community::{Communitys, CommunityNew, CommunityNames, CommunityCategorys, CommunityThemes, CommunityLike};
 
-pub fn community_new(community_new: Json<CommunityNew>, state: State<AppState>) -> FutureResponse<HttpResponse> {
-    state.db.send(CommunityNew{ 
-            create_user_id: community_new.create_user_id,
-            community_name: community_new.community_name.clone(),
-            community_category: community_new.community_category.clone(),
-        })
+pub fn community_new(
+    (community_new, state): (Json<CommunityNew>, State<AppState>),
+) -> FutureResponse<HttpResponse> {
+    state.db.send(CommunityNew {
+        create_user_id: community_new.create_user_id,
+        community_name: community_new.community_name.clone(),
+        community_category: community_new.community_category.clone(),
+    })
         .from_err()
         .and_then(|res| {
             match res {
@@ -19,10 +21,12 @@ pub fn community_new(community_new: Json<CommunityNew>, state: State<AppState>) 
         }).responder()
 }
 
-pub fn community_names(community_names: Json<CommunityNames>, state: State<AppState>) -> FutureResponse<HttpResponse> {
-    state.db.send(CommunityNames{ 
-            create_user_id: community_names.create_user_id,
-        })
+pub fn community_names(
+    (community_names, state): (Json<CommunityNames>, State<AppState>),
+) -> FutureResponse<HttpResponse> {
+    state.db.send(CommunityNames {
+        create_user_id: community_names.create_user_id,
+    })
         .from_err()
         .and_then(|res| {
             match res {
@@ -57,9 +61,9 @@ pub fn communitys(req: HttpRequest<AppState>) -> FutureResponse<HttpResponse> {
 pub fn community_theme_list(req: HttpRequest<AppState>) -> FutureResponse<HttpResponse> {
     let header_community_name = req.match_info().get("community_name").unwrap();
     let community_name = header_community_name.to_string();
-    req.state().db.send(CommunityThemes{
+    req.state().db.send(CommunityThemes {
         community_name: community_name,
-        })
+    })
         .from_err()
         .and_then(|res| {
             match res {
@@ -69,7 +73,9 @@ pub fn community_theme_list(req: HttpRequest<AppState>) -> FutureResponse<HttpRe
         }).responder()
 }
 
-pub fn community_like(community_like: Json<CommunityLike>, state: State<AppState>) -> FutureResponse<HttpResponse> {
+pub fn community_like(
+    (community_like, state): (Json<CommunityLike>, State<AppState>),
+) -> FutureResponse<HttpResponse> {
     state.db.send(CommunityLike {
         user_id: community_like.user_id,
         community_name: community_like.community_name.clone(),
